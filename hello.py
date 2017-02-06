5f15f4dd1e5061d2deceebac6a3399f23a471711
koperkuba <koperkuba@gmail.com>
2017-02-06 14:09:02 +0100

    First lesson

--- hello.py
+++ hello.py
@@ -1,2 +1,10 @@
-print('Witaj świecie! To mój pierwszy program w Pythonie! Ha!')
-print('Chyba potrafię używać GIT')
+# This program says hello and asks for my name.
+
+print('What is tour name? ') # aks for their name
+myName = input()
+print('Its is good to meet You, ' + myName)
+print('The length of your name is: ')
+print(len(myName))
+print('What is your age?') #ask for their age
+myAge = input()
+print('You will be ' + str(int(myAge) + 1)+ ' in a year.')
