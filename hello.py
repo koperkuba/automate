38d079716de037bd5476b72d642c3a07580df9ab
koperkuba <koperkuba@gmail.com>
2017-02-06 14:30:12 +0100

    First lesson

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
 # This program says hello and asks for my name.
 
-print('What is tour name? ') # aks for their name
+print('What is your name? ') # aks for their name
 myName = input()
 print('Its is good to meet You, ' + myName)
 print('The length of your name is: ')
 print(len(myName))
-print('What is your age?') #ask for their age
+print('What is your age?') # ask for their age
 myAge = input()
-print('You will be ' + str(int(myAge) + 1)+ ' in a year.')
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
